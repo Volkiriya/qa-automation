0f6df4def0fb926d3d1e621a0f77d3c7003a8e39
Aleksandra <Maplewood3300@yandex.ru>
2025-08-27 23:01:55 +0300

    add firsts LoginPage tests

---
+++ shopsphere-ui/src/main/java/pages/LoginPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import com.codeborne.selenide.Selenide;
+import static com.codeborne.selenide.Selenide.$;
+import com.codeborne.selenide.SelenideElement;
+
+public class LoginPage {
+    public LoginPage open() {
+        Selenide.open("/");
+        return this;
+    }
+
+    public LoginPage login(String username, String password) {
+        $("#user-name").setValue(username);
+        $("#password").setValue(password);
+        $("#login-button").click();
+        return this;
+    }
+
+    public SelenideElement error() {
+        return $("[data-test-id='error']");
+    }
+
+}
--- shopsphere-ui/src/test/java/LoginPageTest.java
+++ shopsphere-ui/src/test/java/LoginPageTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import pages.LoginPage;
 
 public class LoginPageTest {
     @BeforeAll
@@ -15,6 +18,22 @@ public class LoginPageTest {
     @Test
     void loginPageHasUsernameField() {
         open("/");
-        $("#user-name").should(exist);
+        $("#user-name").setValue("standard_user");
+        $("#password").setValue("secret_sauce");
+        $("#login-button").click();
+        $(".inventory_list").shouldBe(visible);
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'','',Username is required",
+            "standard_user,'',Password is required",
+            "standard_user,bad,Username and password do not match",
+    })
+    void login_negative(String user, String pass, String expected) {
+        LoginPage page = new LoginPage().open();
+        page.login(user, pass);
+        page.error().shouldHave(text(expected));
+    }
+
 }
