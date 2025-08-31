c69fcfaf11819da36b528fdf046b6f0327efa280
Aleksandra <Maplewood3300@yandex.ru>
2025-08-31 23:25:06 +0300

    chore: setup junit5 + selenide

---
+++ shopsphere-ui/src/test/java/qa/shopsphere/ui/LoginSmokeTest.java
@@ -0,0 +1,17 @@
+package qa.shopsphere.ui;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class LoginSmokeTest {
+    @Test @Tag("smoke")
+    void opensLoginPage() {
+
+    }
+
+    @Test
+    @Tag("smoke")
+    void canLoginWithValidCreds() {
+
+    }
+}
