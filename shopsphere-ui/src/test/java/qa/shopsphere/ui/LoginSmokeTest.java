6f08107cdcadbc218bb0c9fcdae0957e4773f1a5
Aleksandra <Maplewood3300@yandex.ru>
2025-08-31 23:42:41 +0300

    add ci

--- shopsphere-ui/src/test/java/qa/shopsphere/ui/LoginSmokeTest.java
+++ shopsphere-ui/src/test/java/qa/shopsphere/ui/LoginSmokeTest.java
@@ -1,12 +1,17 @@
 package qa.shopsphere.ui;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+import static com.codeborne.selenide.Selenide.open;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class LoginSmokeTest {
     @Test @Tag("smoke")
-    void opensLoginPage() {
-
+    void openLoginPage() {
+        open("/");
+        $("#user-name").shouldBe(visible);
     }
 
     @Test
